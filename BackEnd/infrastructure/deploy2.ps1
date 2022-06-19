7a0e0cebf2721e3ed46c3d52c84a6bd72e03b4ac
Najib Radzuan <najibradzuan@devops4me.com>
2022-06-20 00:47:29 +0800

    updated  BackEnd_20220620004

--- BackEnd/infrastructure/deploy2.ps1
+++ BackEnd/infrastructure/deploy2.ps1
@@ -4,6 +4,10 @@ param(
     )
 
 . $PSScriptRoot\common.ps1
+
+$ErrorActionPreference = "Stop" #this ensures that script stops executing at first error in the script.
+
+
 Write-host "Installing nuget package provider"
 Install-PackageProvider -Name NuGet -Scope CurrentUser -MinimumVersion 2.8.5.201 -Force
 Write-Host "Setting Power Shell gallery"
@@ -30,22 +34,24 @@ Write-Host "Importing PowerShell modules completed."
 #This is required due to an issue due to which PowerShell fails to connect with online resources. This issue is machine specific. So you can comment it if not required.
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Write-Host "Connecting to Azure account..."
-Connect-AzAccount
+Connect-AzAccount 
 
-<#
-What is the structure of the ZIP file?
---------------------------------------
-The ZIP file should contain the assemblies at the very top level, i.e. not in any subfolder
-How to publish?
----------------
-dotnet publish  --configuration Release --output %temp%\azdevopsautomecicd WebApplication1.csproj
-#>
-#Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
-$pathWebAppZip= '$(System.DefaultWorkingDirectory)/_BackEnd-Build/webappzip/WebApplication1.zip'
-#$ctx=Get-AzContext
-# az webapp deploy --name $WebAppName --resource-group $ResourceGroup --src-path $pathtozip --type zip --subscription $ctx.Subscription.Id
+Write-Host "Setting subscription for current session..."
+az account set --subscription $subscription
+
+# Write-Host "Creating resource group..."
+# az group create --name $resourceGroup --location $location
+
+# $webAppName="WelcomeCloudApp"  
+# $appServicePlan="WelcomeCloudAppServicePlan"  
+
+# Write-Host "Creating WebApp Service plan..."
+# New-AzAppServicePlan  -Name $appServicePlan -ResourceGroupName $resourceGroup  -Location $location -Tier 'Free' #-Debug  
+
+# Write-Host "Creating WebApp..."
+# New-AzWebApp -Name $webAppName -Location $location -AppServicePlan $appServicePlan -ResourceGroupName $resourceGroup 
+
 Write-Host "Publishing WebApp..."
-Publish-AzWebApp -ResourceGroupName $resourceGroup -Name $webAppName -ArchivePath $pathWebAppZip
-# az webapp start --name $WebAppName --resource-group $ResourceGroup  --subscription $ctx.Subscription.Id
-Write-Host "Starting WebApp..."
-Start-AzWebApp -ResourceGroupName $ResourceGroup -Name $WebAppName
+Publish-AzWebApp -ResourceGroupName $resourceGroup -Name $webAppName -ArchivePath WelcomeCloudAppService.zip  
+
+Write-Host "Finished installing your web app. Bye!"
