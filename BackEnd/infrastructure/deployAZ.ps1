1bce15c482aa85a4d548938946145ecfca545994
Najib Radzuan <najibradzuan@devops4me.com>
2022-06-21 10:28:54 +0800

    updated Backend and Frontend_20220621003

--- BackEnd/infrastructure/deployAZ.ps1
+++ BackEnd/infrastructure/deployAZ.ps1
@@ -1,10 +1,5 @@
 . $PSScriptRoot\common.ps1
-param(
-    [Parameter(Mandatory=$true)]
-    [string]$pathtozip
-    )
-
 Write-Host "Publishing WebApp..."
-az webapp deploy --resource-group $ResourceGroup --name $WebAppName --src-path $pathtozip --type zip --async true
+az webapp deploy --resource-group $ResourceGroup --name $WebAppName --src-path $(pathtozip) --type zip --async true
 Write-Host "Starting WebApp..."
 az webapp start --name $WebAppName --resource-group $ResourceGroup
