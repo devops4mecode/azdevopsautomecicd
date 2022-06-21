925273013f4638df709cad6dfbe7507b7498414f
Najib Radzuan <najibradzuan@devops4me.com>
2022-06-21 10:13:04 +0800

    updated Backend and Frontend_20220621002

--- BackEnd/infrastructure/deploy2.ps1
+++
@@ -1,6 +0,0 @@
-. $PSScriptRoot\common.ps1
-
-Write-Host "Publishing WebApp..."
-az webapp deploy --resource-group $ResourceGroup --name $WebAppName --src-path $(pathtozip) --type zip --async true
-Write-Host "Starting WebApp..."
-az webapp start --name $WebAppName --resource-group $ResourceGroup
